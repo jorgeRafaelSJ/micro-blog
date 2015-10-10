ebb91c2ef3dbfabd36e470b90e1ca8f8cdf7fdb9
jorgeRafaelSJ <jorgerafaelsj@gmail.com>
2015-10-09 19:58:29 -0700

    Added post form and submit button, and created append function to take input and log it in the post section

--- base.js
+++ base.js
@@ -1,7 +1,11 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
+	//appending the text from the input box into the content
+	$("#postButton").on("click", function(e){
+		e.preventDefault();
 
-  // code in here
+		$("#postbox").append( "<p>" + $("#postEntry").val() + "</p>");
 
+	});
 });
