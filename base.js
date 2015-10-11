4dcad051c035be31d41d8956dc0e05b8227b5377
jorgeRafaelSJ <jorgerafaelsj@gmail.com>
2015-10-11 15:25:20 -0700

    added some list styling  and append function for posts

--- base.js
+++ base.js
@@ -1,11 +1,38 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
-	//appending the text from the input box into the content
+
+	var content;
+	var time;
+	var count = 0;
+
 	$("#postButton").on("click", function(e){
 		e.preventDefault();
 
-		$("#postbox").append( "<p>" + $("#postEntry").val() + "</p>");
+		text= $("#postEntry").val();
+		time = new Date();
 
+
+		//appending the text from the input form into the list with button to remove if desired.
+		if(text === ""){
+  			//add alert?
+		} else {
+			$("#post-list").append(
+				"<div class = 'row col-xs-offset-1'><li class='list-group-item col-xs-10'>" + time +"<br>&nbsp" + text +"</li>" +
+				"<button type='submit' class='btn btn-default delete-post' aria-label='Right Align'>" +
+				"<span class='glyphicon glyphicon-remove' aria-hidden='true'></span></button'></div>"
+				);
+
+			count ++;
+		}
+		//reset post input form
+		$("#postEntry").val("");
+	});
+
+	//delete button on the right side of every post
+	$(".delete-post").on("click", function(e){
+		e.preventDefault();
+		$(".list-group-item").remove();
 	});
 });
+
